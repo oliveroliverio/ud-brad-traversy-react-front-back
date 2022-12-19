aed704c3b40eb712f97182e7f78644eef45fb8aa
Oliver Oliverio <oliveroliverio@gmail.com>
2022-12-19 14:25:12 -0800

    practice jsx

--- src/App.js
+++ src/App.js
@@ -1,37 +1,37 @@
 import React from 'react'
 
-// function App() {
-//   const title = 'test title'
-//   const body = 'test body'
-//   const comments = [
-//     { id: 1, text: 'comment one' },
-//     { id: 2, text: 'comment two' },
-//     { id: 3, text: 'comment three' },
-//     { id: 4, text: 'comment four' },
-//   ]
-//   const showComments = false
+function App() {
+  const title = 'test title'
+  const body = 'test body'
+  const comments = [
+    { id: 1, text: 'comment one' },
+    { id: 2, text: 'comment two' },
+    { id: 3, text: 'comment three' },
+    { id: 4, text: 'comment four' },
+  ]
+  const showComments = false
 
-//   const commentBlock = (
-//     <div className='comments'>
-//       <h3>Comments ({comments.length})</h3>
-//       <ul>
-//         {comments.map((comment, index) => (
-//           <li key={index}>{comment.text}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
+  const commentBlock = (
+    <div className='comments'>
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
 
-//   return (
-//     <>
-//       <div>
-//         <div className='container'>
-//           <h1>{title.toUpperCase()}</h1>
-//           <p>{body}</p>
-//           {showComments && commentBlock}
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-// export default App
+  return (
+    <>
+      <div>
+        <div className='container'>
+          <h1>{title.toUpperCase()}</h1>
+          <p>{body}</p>
+          {showComments && commentBlock}
+        </div>
+      </div>
+    </>
+  )
+}
+export default App
