f5c0c1c25d87e978e699792b31843dd469b222b9
Oliver Oliverio <oliveroliverio@gmail.com>
2022-12-19 14:24:21 -0800

    practice jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// function App() {
+//   const title = 'test title'
+//   const body = 'test body'
+//   const comments = [
+//     { id: 1, text: 'comment one' },
+//     { id: 2, text: 'comment two' },
+//     { id: 3, text: 'comment three' },
+//     { id: 4, text: 'comment four' },
+//   ]
+//   const showComments = false
 
-export default App;
+//   const commentBlock = (
+//     <div className='comments'>
+//       <h3>Comments ({comments.length})</h3>
+//       <ul>
+//         {comments.map((comment, index) => (
+//           <li key={index}>{comment.text}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+
+//   return (
+//     <>
+//       <div>
+//         <div className='container'>
+//           <h1>{title.toUpperCase()}</h1>
+//           <p>{body}</p>
+//           {showComments && commentBlock}
+//         </div>
+//       </div>
+//     </>
+//   )
+// }
+// export default App
